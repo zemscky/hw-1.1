ac49fe50e075e875ced20f0cd646d078f8a9f42d
zemscky <aleksei.zemscky@yandex.ru>
2022-09-22 14:23:16 +0300

    Домашняя работа 1, задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Домашнее задание 1
+        System.out.println("Домашнее задание 1");
+        // Задание 1.1
+        System.out.println("Задание 1.1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
